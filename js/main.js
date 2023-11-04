1722f63aa0672a45c2c7a430ac2f3f036b2366db
PinoGabriel <pinogabriel01@gmail.com>
2023-11-04 15:07:19 +0100

    feat: add li inside ul

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const list = ["pomodoro", "latte", "fragole", "pane","verdura", "acqua", "pasta"
 
 console.log(list);
 
-let i = 0
+/* let i = 0
 let element = ""
 
 while (i < list.length) {
@@ -15,5 +15,22 @@ while (i < list.length) {
     
     
     document.getElementById("shoppingList").innerHTML += element
+    i++
+} */
+
+
+let i = 0
+
+const ul = document.createElement("ul")
+const shopping = document.getElementById("shoppingList");
+
+while (i < list.length) {
+    const element = list[i];
+    let li = document.createElement("li")
+    li.innerHTML = element
+
+    shopping.append(ul)
+    ul.append(li)
+
     i++
 }
